1c135cc239136d63848c4ea0d6ec6f12544322dc
Wildan Faris <80156120+wildan-faris@users.noreply.github.com>
2023-01-17 11:58:55 +0800

    Update index.jsx

--- cocktail/components/Navbar/index.jsx
+++ cocktail/components/Navbar/index.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
           <a href="/">
             <img src="/cocktail-nav.png" alt="cocktail" width={60} />
           </a>
-          <h1 className="text-white font-semibold text-lg ml-4 md:ml-8 uppercase text-center">Cocktails Menu Search App</h1>
+          <h1 className="text-white font-semibold text-lg ml-4 md:ml-8 uppercase text-center">Drinks Menu Search App</h1>
         </div>
       </div>
     </nav>
